239288dbface8e6d27c8584ad90437ba1f84ccf7
upconett <upconett@gmail.com>
2024-02-11 22:28:31 +0300

    Token BugFix #2

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 
-TOKEN = open('token.txt').readline()
+TOKEN = open('token.txt').readline().replace('\n','').replace(' ','')
+print(TOKEN)
 
 bot = Bot(token = TOKEN, parse_mode='HTML')
 dp = Dispatcher()
