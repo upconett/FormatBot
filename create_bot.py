53b89d0b5e3a697e5671ee9e1ac685b70d18058e
upconett <upconett@gmail.com>
2024-02-12 01:27:45 +0600

    FileDir Creation

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,9 @@
 from aiogram import Bot, Dispatcher
+import os
 
 TOKEN = open('token.txt').readline()
+if not os.path.isdir('files'):
+	os.makedirs('files')
 
 bot = Bot(token = TOKEN, parse_mode='HTML')
 dp = Dispatcher()
